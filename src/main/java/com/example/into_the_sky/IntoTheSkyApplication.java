5fcc1efde27702846964eb749c58caf0fe8fad13
Marta183 <marta.ilchenko95@gmail.com>
2025-05-20 18:15:48 +0200

    init commit - spring io

---
+++ src/main/java/com/example/into_the_sky/IntoTheSkyApplication.java
@@ -0,0 +1,13 @@
+package com.example.into_the_sky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntoTheSkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntoTheSkyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/into_the_sky/IntoTheSkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.into_the_sky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntoTheSkyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
